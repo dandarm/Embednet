a73b591592741a93acdf5436d2d5a2923e9b0db3
dandarm <daniele.darmiento@gmail.com>
2022-06-09 15:23:54 +0200

    Observe correlation...

--- graph_generation.py
+++ graph_generation.py
@@ -11,16 +11,19 @@ def create_ER(Num_nodes, p, N_graphs):
     N_graphs: ogni tot abbiamo un tipo di grafi originato da una diversa distribuzione
     """
     grafi = []
+    probs = []
     for i in range(N_graphs):
         gr = nx.erdos_renyi_graph(Num_nodes, p)  # seed = 1
         #grafi.append(nx.to_numpy_matrix(gr))
         grafi.append(gr)
+        actual_p = nx.to_numpy_matrix(gr).sum(axis=1).mean() / (Num_nodes - 1)
+        probs.append(actual_p)
         
     print("Mean connectivity for each node:", end=' ')    
     print(round(np.array([nx.to_numpy_matrix(t).sum(axis=1).mean() for t in grafi]).mean(), 3), end= ' ')
     print(f'p={p}')
         
-    return grafi
+    return grafi, probs
 
 # def dataset_2class_ER(config):
 #     N = config['graph_dataset']['Num_nodes']
@@ -50,13 +53,20 @@ def dataset_nclass_ER(config):
     else:
         scalar_embedding = True
 
-    for i, p in enumerate(list_p):
-        grafi_p = create_ER(N, p, Num_grafi_per_tipo)
-        dataset_grafi_nx = dataset_grafi_nx + grafi_p
-        if not scalar_embedding: # se l'embedding ha una sola dimensione la label deve essere uguale alla probabilità
+    if not scalar_embedding:  # se l'embedding ha più di una sola dimensione la label è un intero o one-hot
+        for i, p in enumerate(list_p):
+            grafi_p, _ = create_ER(N, p, Num_grafi_per_tipo)
+            dataset_grafi_nx = dataset_grafi_nx + grafi_p
             dataset_labels = dataset_labels + [i] * len(grafi_p)
-        else:
-            dataset_labels = dataset_labels + [p] * len(grafi_p)
-    print(f'Labels values: {set(dataset_labels)}')
+        print(f'Labels values: {set(dataset_labels)}')
+
+    else:  # swe l'embedding è scalare devo calcolare la probabilità attuale del grafo,
+           # non quella che ho impostato alla generazione
+        for i, p in enumerate(list_p):
+            grafi_p, probs = create_ER(N, p, Num_grafi_per_tipo)
+            dataset_grafi_nx = dataset_grafi_nx + grafi_p
+            for p in probs:
+                dataset_labels = dataset_labels + [p]
+        #print(f'Labels values: {set(dataset_labels)}')
         
     return dataset_grafi_nx, np.array(dataset_labels), list_p
